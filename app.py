886b8959c6119aa2e9cc1dd31df24367c0811241
jai12kushwah <g23ai2018@iitj.ac.in>
2024-09-22 14:19:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/', methods = ['get'])
 
 def home():
-  return ("Hello welcome to VCC_Project by G23ai2026")
+  return ("Hello welcome to VCC_Project by G23ai2018- Jai SIngh Kushwah")
 if __name__== "__main__":
  app.run(debug=True,host="0.0.0.0", port = 5000)
